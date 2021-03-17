3610921320e27e277860572387d6c5cb3d4cda82
maumalik <maumalik508@gmail.com>
2021-03-17 15:25:04 +0700

    menambahkan postcontroller

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Like;
 
 class Post extends Model
 {
@@ -21,5 +22,15 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get all of the comments for the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,22 +2,24 @@
 @section('content')
     <div class="flex justify-center">
         <div class="p-6 w-8/12 bg-white rounded-lg shadow-md">
-            <form action="{{ route('posts') }}" method="post" class="mb-4">
-            @csrf
-            <div class="mb-4">
-                <label for="body" class="sr-only">Body</label>
-                <textarea name="body" id="body" cols="30" rows="10" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post Here"></textarea>
+            @auth     
+                <form action="{{ route('posts') }}" method="post" class="mb-4">
+                @csrf
+                <div class="mb-4">
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea name="body" id="body" cols="30" rows="10" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post Here"></textarea>
 
-                @error('body')
-                    <div class='text-red-500 mt-1 text-xs'>
-                        {{$message}}
-                    </div>
-                @enderror
-            </div>
-            <div>
-                <Button type="submit" class="bg-blue-500 text-white py-2 px-6 rounded font-medium">Post</Button>
-            </div>
-            </form>
+                    @error('body')
+                        <div class='text-red-500 mt-1 text-xs'>
+                            {{$message}}
+                        </div>
+                    @enderror
+                </div>
+                <div>
+                    <Button type="submit" class="bg-blue-500 text-white py-2 px-6 rounded font-medium">Post</Button>
+                </div>
+                </form>
+            @endauth
 
             @if ($posts->count())
                 @foreach ($posts as $post)
@@ -25,17 +27,21 @@
                         <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-xs text-gray-400">{{ $post->created_at->diffForHumans() }}</span>
                         <p>{{ $post->body }}</p>
 
-                        <div class="flex item-center">
-                            <form action="" method="post" class="mr-2">
-                                @csrf
-                                <button type="submit" class="text-blue-500">like</button>
-                            </form>
+                        @auth    
+                            <div class="flex item-center">
+                                <form action="{{ route('posts.likes', $post->id)}}" method="post" class="mr-2">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">like</button>
+                                </form>
 
-                            <form action="" method="post" class="mr-2">
-                                @csrf
-                                <button type="submit" class="text-blue-500">unlike</button>
-                            </form>
-                        </div>
+                                <form action="" method="post" class="mr-2">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">unlike</button>
+                                </form>
+
+                                <span>{{ $post->likes->count() }}</span>
+                            </div>
+                        @endauth
                     </div>
                 @endforeach
 
